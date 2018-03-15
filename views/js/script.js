0d5c8794b924dd09bb5dd25875026dde603abdcf
Gökberk <gokberknur@gmail.com>
2018-03-15 13:17:17 +0100

    Update script.js
    15 march update : google maps marker

--- views/js/script.js
+++ views/js/script.js
@@ -1,5 +1,7 @@
 var timeout = null;
 //Get pharmacy data from server
+
+
 function getData(){
    $.ajax({
      url: '/geoList',
@@ -7,18 +9,38 @@ function getData(){
      dataType: 'json',
      contentType: 'application/json',
      success: function(response) {
+       
        console.log(response);
+      var ourList = response.geoList;
+     
+      for(var i=0, len=ourList.length; i<len; i++) {
+          
+      var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(ourList[i].a,ourList[i].b),
+        title: ourList[i].l,
+       map:map
+        
+        
+   }); 
+                                          
+   } 
+   
+
+         
      }
+
    });
  };
 
 //Send coordinates to server
 $(document).on('click', '#send-coords', function(){
+  
   var pendingCall = { timeStamp: null, procID: null };
   var longInput = $('.long').text();
   var latInput = $('.lat').text();
   var coords = longInput + ', ' + latInput;
   console.log(JSON.stringify(longInput) + ' ' + JSON.stringify(latInput));
+  
   $.ajax({
     url: '/resList',
     method: 'POST',
@@ -33,4 +55,8 @@ $(document).on('click', '#send-coords', function(){
   if(timeout) clearTimeout(timeout);
   timeout = setTimeout(getData, 2000);
 
+  
+ 
+
+  
 });
